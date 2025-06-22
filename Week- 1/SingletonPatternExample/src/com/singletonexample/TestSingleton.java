df8484083e28ab1d940f6f82424a6ae10a8fc55f
TusharJain244 <139066569+TusharJain244@users.noreply.github.com>
2025-06-22 23:01:22 +0530

    TestSingleton.java

--- Week- 1/SingletonPatternExample/src/com/singletonexample/TestSingleton.java
+++ Week- 1/SingletonPatternExample/src/com/singletonexample/TestSingleton.java
@@ -1,26 +1,25 @@
-// src/com/singletonexample/TestSingleton.java
 package com.singletonexample;
 
-import com.singletonexample.Logger; // <--- Add this line
+import com.singletonexample.Logger; 
 
 public class TestSingleton {
     public static void main(String[] args) {
-        // Get the first instance of Logger
+        
         System.out.println("Attempting to get logger1...");
         Logger logger1 = Logger.getInstance();
         logger1.log("This is the first log message.");
 
-        // Get the second instance of Logger
+        
         System.out.println("\nAttempting to get logger2...");
         Logger logger2 = Logger.getInstance();
         logger2.log("This is the second log message.");
 
-        // Get the third instance of Logger
+    
         System.out.println("\nAttempting to get logger3...");
         Logger logger3 = Logger.getInstance();
         logger3.log("This is the third log message.");
 
-        // Verify that all instances are the same
+        
         System.out.println("\n--- Verifying Instances ---");
         System.out.println("logger1 == logger2: " + (logger1 == logger2));
         System.out.println("logger1 == logger3: " + (logger1 == logger3));
